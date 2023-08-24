72da5a7f725037dd884748c5a0bcecd41aeeb20c
SelesPurple <106119819+SelesPurple@users.noreply.github.com>
2023-08-24 11:24:47 +0300

    add dragstart and dragend

--- 2 drag&drop/app.js
+++ 2 drag&drop/app.js
@@ -1,1 +1,12 @@
- 
+const item = document.querySelector('.item') // такой метод потому что нужен всего один элемент
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+function dragstart(event) {
+    console.log('drag start!!!!', event.target) //event target это и есть элемент
+    event.target.classList.add('hold')
+}
+function dragend(event) {
+    console.log('drag end!!!!')
+    event.target.classList.revove('hold')
+}
