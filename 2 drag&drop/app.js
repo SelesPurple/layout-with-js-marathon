e79d173a7fe1d3758bfa20b8ed263c3a13da117f
SelesPurple <106119819+SelesPurple@users.noreply.github.com>
2023-08-24 12:14:21 +0300

    проблемы с событием dragdrop

--- 2 drag&drop/app.js
+++ 2 drag&drop/app.js
@@ -1,12 +1,40 @@
 const item = document.querySelector('.item') // такой метод потому что нужен всего один элемент
+const placeholders =  document.querySelectorAll('.placeholder')
+
 item.addEventListener('dragstart', dragstart)
 item.addEventListener('dragend', dragend)
 
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover) // если элемент над плейсом
+    placeholder.addEventListener('dragenter', dragenter) // элемент на территории плейса 
+    placeholder.addEventListener('dragleave', dragleave) // элемент перетащили, но вышли
+    placeholder.addEventListener('dragdrop', dragdrop) // отпустили
+}
 function dragstart(event) {
-    console.log('drag start!!!!', event.target) //event target это и есть элемент
+     //event target это и есть элемент
     event.target.classList.add('hold')
+    setTimeout(() => 
+        event.target.classList.add('hide'), 0) //функция будет вызвана после выполнения синхронноого кода, даже если в таймере 0
 }
 function dragend(event) {
-    console.log('drag end!!!!')
-    event.target.classList.revove('hold')
+    event.target.classList.remove('hold', 'hide')
+}
+
+function dragover (event) {
+    event.preventDefault()
+}
+
+function dragenter (event) {
+    event.target.classList.add('hovered')
+
+}
+
+function dragleave (event) {
+    event.target.classList.remove('hovered')
+
+}
+
+function dragdrop (event) {
+    event.target.append(item)
 }
