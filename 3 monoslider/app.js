6c9cea9c9aa766558a0530fd6d3b1fe44ba2c3bd
SelesPurple <106119819+SelesPurple@users.noreply.github.com>
2023-08-25 14:13:44 +0300

    в первом слайде надо что-то придумать со стилем
    transition еще поменять, чтобы город был быстрее чем описание

--- 3 monoslider/app.js
+++ 3 monoslider/app.js
@@ -5,15 +5,21 @@ const sidebar = document.querySelector('.sidebar')
 
 const mainSlide = document.querySelector('.main-slide')
 const slidesCount = mainSlide.querySelectorAll('div').length
+
+
 sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
 
 upBtn.addEventListener('click', () => {
-
+    changeSlide('up')
 })
 
 downBtn.addEventListener('click', () => {
-    
+    changeSlide('down')
 })
+
+let activeSlideIndex = 0 
+const container = document.querySelector('.container')
+
 
 function changeSlide (direction) {
     if (direction === 'up') {
@@ -21,12 +27,16 @@ function changeSlide (direction) {
         if (activeSlideIndex === slidesCount) {
             activeSlideIndex = 0
         }
-        else if (direction === 'down') {
-            activeSlideIndex--
-            if (activeSlideIndex < 0) {
-                activeSlideIndex = slidesCount - 1
-            }
-        }
-        
     }
+    else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1
+        }
+    }
+    const height = container.clientHeight
+    
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
 }
